281b7c2f0e0bc74836dd002d58f5e54fc53500e4
KorotunYura <yurakorotun1991@gmail.com>
2023-12-15 08:44:42 +0200

    Update 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(handlerTime, 1000));
+
+function handlerTime(data) {
+  const time = data.seconds;
+
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+}
+
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {});
